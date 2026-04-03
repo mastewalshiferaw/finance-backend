9c1a3efead2aaab7aff168b79df2661c8f53afe8
Mastewal <mastewalgshiferaw@gmail.com>
2026-04-03 09:20:52 +0300

    Feature: Add TransactionSerializer with validation

---
+++ records/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Transaction
+
+
+class TransactionSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+        read_only_fields = ['user'] #automatically a user will be set automatically in the view
+
+        def validate_amount(self, value):
+            if value <=0:
+                raise serializers.ValidationError("Amount must be a positive number.")
+            return value

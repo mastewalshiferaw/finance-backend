fe631492fb3fb3b401dcf1f19d2ec33af51d33cb
Mastewal <mastewalgshiferaw@gmail.com>
2026-04-03 09:49:40 +0300

    Feature: Add TransactionViewSet with filtering

--- records/views.py
+++ records/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Transaction
+from .serializers import TransactionSerializers
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializers
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_querysert(self):
+        #Users can only see their own records 
+        #return Transaction.objects.all() if Admins see everything
+
+        return Transaction.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        # Automatically assign the loggen-in user to the record
+        serializer.save(user=self.request.user)
+        
